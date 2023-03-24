421ec1f8d7a9bba5eb159521a2a41be37d4a5b4a
hk14004 <hardijs.kirsis@gmail.com>
2023-03-24 20:48:20 +0200

    Add initial domain models

---
+++ CatalogViewer/Data/Models/Domain/Category/Category.swift
@@ -0,0 +1,16 @@
+//
+//  Category.swift
+//  CatalogViewer
+//
+//  Created by Hardijs Ķirsis on 24/03/2023.
+//
+
+import Foundation
+
+struct Category {
+    let id: String
+    let parentID: String
+    let imageURL: String
+    let size: String
+    let title: String
+}
---
+++ CatalogViewer/Data/Models/Domain/Product/Product.swift
@@ -0,0 +1,25 @@
+//
+//  Product.swift
+//  CatalogViewer
+//
+//  Created by Hardijs Ķirsis on 24/03/2023.
+//
+
+import Foundation
+
+struct Product {
+    let id: String
+    let mainCategoryID: String
+    let type: String
+    let typeName: String
+    let title: String
+    let brand: String
+    let model: String
+    let image: String
+    let variantCount: String
+    let currencyID: String
+    
+    // Files?
+    // Options?
+    // Techniques?
+}
