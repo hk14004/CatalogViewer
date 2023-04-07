f2e7fc279389ed0a600b3dbec60f20149f5c0615
hk14004 <hardijs.kirsis@gmail.com>
2023-04-07 20:24:12 +0300

    Shit

---
+++ CatalogViewer/Config/DI/Dependencies + Realm.swift
@@ -0,0 +1,29 @@
+//
+//  Dependencies + CoreData.swift
+//  CatalogViewer
+//
+//  Created by Hardijs Ķirsis on 07/04/2023.
+//
+
+import Swinject
+import CoreData
+import RealmSwift
+import DevToolsRealm
+import DevTools
+
+let DI_REALM: (Container)->() = { container in
+    
+//    container.register(Realm.Configuration.self) { resolver in
+//        Realm.Configuration()
+//    }
+//    container.register(BasePersistedLayerInterface<Category>.self) { resolver in
+//        PersistentRealmStore<Category>(dbConfig: resolver.resolve(Realm.Configuration.self)!)
+//    }
+//    container.register(BasePersistedLayerInterface<Product>.self) { resolver in
+//        PersistentRealmStore<Product>(dbConfig: resolver.resolve(Realm.Configuration.self)!)
+//    }
+//    container.register(BasePersistedLayerInterface<ProductVariant>.self) { resolver in
+//        PersistentRealmStore<ProductVariant>(dbConfig: resolver.resolve(Realm.Configuration.self)!)
+//    }
+    
+}
