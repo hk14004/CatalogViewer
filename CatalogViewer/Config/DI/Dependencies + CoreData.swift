9953ab5da4367d4b971b0f30db7c206caa24b368
hk14004 <hardijs.kirsis@gmail.com>
2023-04-08 16:49:24 +0300

    Recreate model

--- CatalogViewer/Config/DI/Dependencies + CoreData.swift
+++ CatalogViewer/Config/DI/Dependencies + CoreData.swift
@@ -21,7 +21,7 @@ let DI_CORE_DATA: (Container)->() = { diContainer in
              application to it. This property is optional since there are legitimate
              error conditions that could cause the creation of the store to fail.
             */
-            let container = NSPersistentContainer(name: "CatalogViewerDB")
+            let container = NSPersistentContainer(name: "Model")
             container.loadPersistentStores(completionHandler: { (storeDescription, error) in
                 if let error = error as NSError? {
                     // Replace this implementation with code to handle the error appropriately.
